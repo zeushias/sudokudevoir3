9411893b0cb3081627512c9034208933d71b03a0
zeuhias <zeushias@zeushias>
2018-06-01 20:22:00 +0000

    DANON Kodzo Apélété commit du 01/06/2018 20h21 GrilleImplTest comme pour l'exercice 2

--- sudokudevoir3/src/test/java/com/zeushias/sudokudevoir3/GrilleImplTest.java
+++ sudokudevoir3/src/test/java/com/zeushias/sudokudevoir3/GrilleImplTest.java
@@ -18,126 +18,374 @@ import static org.junit.Assert.*;
  */
 public class GrilleImplTest {
     
-    public GrilleImplTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+    /**
+     * Variable statique permettant de manipuler plus facilement les caracteres
+     * vides.
+     */
+    static final char EMPTY = Grille.EMPTY;
+
+    /**
+     * Grille vide.
+     */
+    static final char[][] GRILLE_VIDE_9 = {
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}};
+
+    /**
+     * Grille incomplete.
+     */
+    static final char[][] GRILLE_INCOMPLETE_9 = {
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, '6', EMPTY, '7', EMPTY},
+        {EMPTY, '7', EMPTY, '9', EMPTY, '4', EMPTY, '3', EMPTY},
+        {'4', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, '1'},
+        {'3', EMPTY, '6', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, '2', '8', EMPTY, '1', '9', EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, '1', EMPTY, '5'},
+        {'1', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, '7'},
+        {EMPTY, '3', EMPTY, '5', EMPTY, '8', EMPTY, '9', EMPTY},
+        {EMPTY, '5', EMPTY, '4', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}};
+
+    /**
+     * Grille complete.
+     */
+    static final char[][] GRILLE_COMPLETE_9 = {
+        {'8', '2', '7', '1', '5', '4', '3', '9', '6'},
+        {'9', '6', '5', '3', '2', '7', '1', '4', '8'},
+        {'3', '4', '1', '6', '8', '9', '7', '5', '2'},
+        {'5', '9', '3', '4', '6', '8', '2', '7', '1'},
+        {'4', '7', '2', '5', '1', '3', '6', '8', '9'},
+        {'6', '1', '8', '9', '7', '2', '4', '3', '5'},
+        {'7', '8', '6', '2', '3', '5', '9', '1', '4'},
+        {'1', '5', '4', '7', '9', '6', '8', '2', '3'},
+        {'2', '3', '9', '8', '4', '1', '5', '6', '7'}};
+
+    /**
+     * Test getDimension(). Dimension grille.
+     */
+    static final int TEST_GETDIMENSION_DIMENSION_GRILLE = 9;
+
+    /**
+     * Test getValue(). Valeur de y.
+     */
+    static final int TEST_GETVALUE_VAL_Y = 8;
+
+    /**
+     * Test getValue(). Index x négatif
+     */
+    static final int TEST_GETVALUE_INDEX_X_NEGATIF = -2;
+
+    /**
+     * Test getValue(). Index x négatif valeur y
+     */
+    static final int TEST_GETVALUE_INDEX_X_NEGATIF_Y = 5;
+
+    /**
+     * Test getValue(). Index y négatif
+     */
+    static final int TEST_GETVALUE_INDEX_Y_NEGATIF = 8;
+
+    /**
+     * Test getValue(). Index y négatif valeur x
+     */
+    static final int TEST_GETVALUE_INDEX_Y_NEGATIF_X = -5;
+
+    /**
+     * Test setValueLeveesDExceptions() pour x. Valeur de x.
+     */
+    static final int TEST_SETVALUE_LEVEE_EXCEPTION_X_VAL_X = 15;
+
+    /**
+     * Test setValueLeveesDExceptions() pour x. Valeur de y.
+     */
+    static final int TEST_SETVALUE_LEVEE_EXCEPTION_X_VAL_Y = 5;
+
+    /**
+     * Test setValueLeveesDExceptions() pour y. Valeur de x.
+     */
+    static final int TEST_SETVALUE_LEVEE_EXCEPTION_Y_VAL_X = 3;
+
+    /**
+     * Test setValueLeveesDExceptions() pour y. Valeur de y.
+     */
+    static final int TEST_SETVALUE_LEVEE_EXCEPTION_Y_VAL_Y = 20;
+
+    /**
+     * Test setValueLeveesDExceptions() caractere interdit. Valeur de x.
+     */
+    static final int TEST_SETVALUE_LEVEE_EXCEPTION_VAL_INTERDITE_X = 3;
+
+    /**
+     * Test setValueLeveesDExceptions() caractere interdit. Valeur de y.
+     */
+    static final int TEST_SETVALUE_LEVEE_EXCEPTION_VAL_INTERDITE_Y = 5;
+
+    /**
+     * Test getValueLeveesDExceptions() pour x. Valeur de x.
+     */
+    static final int TEST_GETVALUE_LEVEE_EXCEPTION_X_VAL_X = 15;
+
+    /**
+     * Test getValueLeveesDExceptions() pour x. Valeur de y.
+     */
+    static final int TEST_GETVALUE_LEVEE_EXCEPTION_X_VAL_Y = 2;
+
+    /**
+     * Test getValueLeveesDExceptions() pour y. Valeur de x.
+     */
+    static final int TEST_GETVALUE_LEVEE_EXCEPTION_Y_VAL_X = 4;
+
+    /**
+     * Test getValueLeveesDExceptions() pour y. Valeur de y.
+     */
+    static final int TEST_GETVALUE_LEVEE_EXCEPTION_Y_VAL_Y = 12;
+
+    /**
+     * Test possibleLeveesDExceptions() pour x. Valeur de x.
+     */
+    static final int TEST_POSSIBLE_LEVEE_EXCEPTION_X_VAL_X = -5;
+
+    /**
+     * Test possibleLeveesDExceptions() pour x. Valeur de y.
+     */
+    static final int TEST_POSSIBLE_LEVEE_EXCEPTION_X_VAL_Y = 5;
+
+    /**
+     * Test setValueLeveesDExceptions() pour y. Valeur de x.
+     */
+    static final int TEST_POSSIBLE_LEVEE_EXCEPTION_Y_VAL_X = 3;
+
+    /**
+     * Test possibleLeveesDExceptions() pour y. Valeur de y.
+     */
+    static final int TEST_POSSIBLE_LEVEE_EXCEPTION_Y_VAL_Y = -4;
+
+    /**
+     * Test possibleLeveesDExceptions() caractere interdit. Valeur de x.
+     */
+    static final int TEST_POSSIBLE_LEVEE_EXCEPTION_VAL_INTERDITE_X = 3;
+
+    /**
+     * Test possibleLeveesDExceptions caractere interdit. Valeur de y.
+     */
+    static final int TEST_POSSIBLE_LEVEE_EXCEPTION_VAL_INTERDITE_Y = 5;
+
+    /**
+     * Test possible. Valeur de x.
+     */
+    static final int TEST_POSSIBLE_VAL_X = 3;
+
+    /**
+     * Test possible. Valeur de y.
+     */
+    static final int TEST_POSSIBLE_VAL_Y = 5;
+
+    /**
+     * Test possible. Valeur de y.
+     */
+    static final int TEST_POSSIBLE_VAL_Y_2 = 4;
+
+    /**
+     * Méthode principale.
+     * @param args arguments de la méthode main
+     */
+    public static void main(final String[] args) {
+        GrilleImplTest grilleTest = new GrilleImplTest();
+        grilleTest.testGetDimension();
+        grilleTest.testSetValue();
+        grilleTest.testGetValue();
+        grilleTest.testSetValueLeveesDExceptions();
+        grilleTest.testGetValueLeveesDExceptions();
+        grilleTest.testComplete();
+        grilleTest.testPossibleLeveesDExceptions();
+        grilleTest.testPossible();
     }
 
     /**
-     * Test of getGrille method, of class GrilleImpl.
+     * Permet de tester la methode getDimension().
      */
     @Test
-    public void testGetGrille() {
-        System.out.println("getGrille");
-        GrilleImpl instance = null;
-        char[][] expResult = null;
-        char[][] result = instance.getGrille();
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public final void testGetDimension() {
+        System.out.println("Test de la methode getDimension() ...");
+        Grille sudoku = new GrilleImpl(GRILLE_VIDE_9);
+        sudoku.setGrille(GRILLE_INCOMPLETE_9);
+        assertEquals("Test dimension sur grille vide.",
+                TEST_GETDIMENSION_DIMENSION_GRILLE, sudoku.getDimension());
     }
 
     /**
-     * Test of getDimension method, of class GrilleImpl.
+     * Permet de tester la methode setValue().
      */
     @Test
-    public void testGetDimension() {
-        System.out.println("getDimension");
-        GrilleImpl instance = null;
-        int expResult = 0;
-        int result = instance.getDimension();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public final void testSetValue() {
+        System.out.println("Test de la methode setValue() ...");
+        Grille sudoku = new GrilleImpl(GRILLE_INCOMPLETE_9);
+        sudoku.setValue(1, 1, '2');
+        char recup = sudoku.getValue(1, 1);
+        assertEquals('2', recup);
     }
 
     /**
-     * Test of setValue method, of class GrilleImpl.
+     * Permet de tester la methode getValue().
      */
     @Test
-    public void testSetValue() {
-        System.out.println("setValue");
-        int x = 0;
-        int y = 0;
-        char value = ' ';
-        GrilleImpl instance = null;
-        instance.setValue(x, y, value);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public final void testGetValue() {
+        System.out.println("Test de la methode getValue() ...");
+        Grille sudoku = new GrilleImpl(GRILLE_INCOMPLETE_9);
+        char recup = sudoku.getValue(2, TEST_GETVALUE_VAL_Y);
+        assertEquals('3', recup);
+        try {
+            char recupIndexXNegatif =
+                    sudoku.getValue(TEST_GETVALUE_INDEX_X_NEGATIF,
+                            TEST_GETVALUE_INDEX_X_NEGATIF_Y);
+            fail("Une IllegalArgumentException aurait "
+                    + "du etre levee pour l'index x");
+        } catch (IllegalArgumentException e) {
+
+        }
+        try {
+            char recupIndexYNegatif =
+                    sudoku.getValue(TEST_GETVALUE_INDEX_Y_NEGATIF_X,
+                            TEST_GETVALUE_INDEX_Y_NEGATIF);
+            fail("Une IllegalArgumentException "
+                    + "aurait du etre levee pour l'index y");
+        } catch (IllegalArgumentException e) {
+
+        }
     }
 
     /**
-     * Test of getValue method, of class GrilleImpl.
+     * Permet de tester la levee d'exceptions pour la methode setValue().
      */
     @Test
-    public void testGetValue() {
-        System.out.println("getValue");
-        int x = 0;
-        int y = 0;
-        GrilleImpl instance = null;
-        char expResult = ' ';
-        char result = instance.getValue(x, y);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public final void testSetValueLeveesDExceptions() {
+        System.out.println("Test levees d'exceptions (valeurs incorrectes pour"
+                + " x, y et la valeur) pour la methode setValue() ...");
+        Grille sudoku = new GrilleImpl(GRILLE_COMPLETE_9);
+
+        try {
+            sudoku.setValue(TEST_SETVALUE_LEVEE_EXCEPTION_X_VAL_X,
+                    TEST_SETVALUE_LEVEE_EXCEPTION_X_VAL_Y, '6');
+            fail("Une IllegalArgumentException aurait du etre levee pour x");
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        try {
+            sudoku.setValue(TEST_SETVALUE_LEVEE_EXCEPTION_Y_VAL_X,
+                    TEST_SETVALUE_LEVEE_EXCEPTION_Y_VAL_Y, '6');
+            fail("Une IllegalArgumentException aurait du etre levee pour y");
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        // Valeur interdite
+        try {
+            sudoku.setValue(TEST_SETVALUE_LEVEE_EXCEPTION_VAL_INTERDITE_X,
+                    TEST_SETVALUE_LEVEE_EXCEPTION_VAL_INTERDITE_Y, 'z');
+            fail("Une IllegalArgumentException aurait "
+                    + "du etre levee pour la valeur");
+        } catch (IllegalArgumentException e) {
+
+        }
     }
 
     /**
-     * Test of complete method, of class GrilleImpl.
+     * Permet de tester la levee d'exceptions pour la methode getValue().
      */
     @Test
-    public void testComplete() {
-        System.out.println("complete");
-        GrilleImpl instance = null;
-        boolean expResult = false;
-        boolean result = instance.complete();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public final void testGetValueLeveesDExceptions() {
+        System.out.println("Test levees d'exceptions (s, y) "
+                + "pour la methode getValue() ...");
+        Grille sudoku = new GrilleImpl(GRILLE_COMPLETE_9);
+        try {
+            char valeur = sudoku.getValue(TEST_GETVALUE_LEVEE_EXCEPTION_X_VAL_X,
+                    TEST_GETVALUE_LEVEE_EXCEPTION_X_VAL_Y);
+            fail("Une IllegalArgumentException aurait du etre levee pour x");
+        } catch (IllegalArgumentException e) {
+
+        }
+        try {
+            char valeur = sudoku.getValue(TEST_GETVALUE_LEVEE_EXCEPTION_Y_VAL_X,
+                    TEST_GETVALUE_LEVEE_EXCEPTION_Y_VAL_Y);
+            fail("Une IllegalArgumentException aurait du etre levee pour y");
+        } catch (IllegalArgumentException e) {
+
+        }
     }
 
     /**
-     * Test of possible method, of class GrilleImpl.
+     * Permet de tester la methode complete().
      */
     @Test
-    public void testPossible() {
-        System.out.println("possible");
-        int x = 0;
-        int y = 0;
-        char value = ' ';
-        GrilleImpl instance = null;
-        boolean expResult = false;
-        boolean result = instance.possible(x, y, value);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public final void testComplete() {
+        System.out.println("Test de la methode complete() ...");
+        Grille sudoku = new GrilleImpl(GRILLE_COMPLETE_9);
+        assertEquals("Test de completude de la grille.", true,
+                sudoku.complete());
+        Grille sudokuIncomplet = new GrilleImpl(GRILLE_INCOMPLETE_9);
+        assertEquals("Test de completude de la grille.", false,
+                sudokuIncomplet.complete());
     }
 
     /**
-     * Test of setGrille method, of class GrilleImpl.
+     * Permet de tester la levee d'exceptions pour la methode possible().
      */
     @Test
-    public void testSetGrille() {
-        System.out.println("setGrille");
-        char[][] grilleTab = null;
-        GrilleImpl instance = null;
-        instance.setGrille(grilleTab);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public final void testPossibleLeveesDExceptions() {
+        System.out.println("Test levees d'exceptions (valeurs incorrectes pour"
+                + " x, y et la valeur) pour la methode setValue() ...");
+        Grille sudoku = new GrilleImpl(GRILLE_COMPLETE_9);
+        try {
+            sudoku.possible(TEST_POSSIBLE_LEVEE_EXCEPTION_X_VAL_X,
+                    TEST_POSSIBLE_LEVEE_EXCEPTION_X_VAL_Y, '6');
+            fail("Une IllegalArgumentException aurait du etre levee pour x");
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        try {
+            sudoku.possible(TEST_POSSIBLE_LEVEE_EXCEPTION_Y_VAL_X,
+                    TEST_POSSIBLE_LEVEE_EXCEPTION_Y_VAL_Y, '6');
+            fail("Une IllegalArgumentException aurait du etre levee pour y");
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        // Valeur interdite
+        try {
+            sudoku.possible(TEST_POSSIBLE_LEVEE_EXCEPTION_VAL_INTERDITE_X,
+                    TEST_POSSIBLE_LEVEE_EXCEPTION_VAL_INTERDITE_Y, 'z');
+            fail("Une IllegalArgumentException aurait du "
+                    + "etre levee pour la valeur");
+        } catch (IllegalArgumentException e) {
+
+        }
+
     }
-    
+
+    /**
+     * Permet de tester la methode possible().
+     */
+    @Test
+    public final void testPossible() {
+        System.out.println("Test de la methode possible() ...");
+        Grille sudoku = new GrilleImpl(GRILLE_INCOMPLETE_9);
+        // La valeur 6 existe deja dans le bloc, donc il n'est pas
+        // possible de la renseigner dans la grille.
+        // On s'attend a ce que la verification avec possible donne false.
+        assertEquals("Test valeur possible", false,
+                sudoku.possible(TEST_POSSIBLE_VAL_X, TEST_POSSIBLE_VAL_Y, '6'));
+        assertEquals("Test valeur possible", false,
+                sudoku.possible(TEST_POSSIBLE_VAL_X, TEST_POSSIBLE_VAL_Y, '1'));
+        assertEquals("Test valeur possible", false,
+                sudoku.possible(TEST_POSSIBLE_VAL_X,
+                        TEST_POSSIBLE_VAL_Y_2, '8'));
+    }
 }
